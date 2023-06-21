b7788a01ca90a9e8185c8a52d9aa500cfc02f68b
azimochilov <azimochilov226@gmail.com>
2023-06-21 15:50:19 +0300

    collectionDtos is done

---
+++ CollectX.Service/DTOs/Collections/CollectionCreationDto.cs
@@ -0,0 +1,15 @@
+﻿using CollectX.Domain.Entities;
+using CollectX.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CollectX.Service.DTOs.Collections;
+public class CollectionCreationDto
+{
+    [Required(ErrorMessage = "Name")]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    [Required(ErrorMessage = "Topic")]
+    public Topic Topic { get; set; }
+    public List<Item> Items { get; set; }
+    public long UserId { get; set; }
+}
---
+++ CollectX.Service/DTOs/Collections/CollectionResultDto.cs
@@ -0,0 +1,13 @@
+﻿using CollectX.Domain.Entities;
+using CollectX.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CollectX.Service.DTOs.Collections;
+public class CollectionResultDto
+{    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Topic Topic { get; set; }
+    public List<Item> Items { get; set; }
+    public long UserId { get; set; }
+}
