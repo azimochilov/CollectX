1922c03cfd2992b10a3df2416a9da2e6d13eb5d9
azimochilov <azimochilov226@gmail.com>
2023-06-21 15:05:26 +0300

    Working with Irepo

--- CollectX.Data/IRepositories/IRepository.cs
+++ CollectX.Data/IRepositories/IRepository.cs
@@ -1,4 +1,13 @@
-﻿namespace CollectX.Data.IRepositories;
-public interface IRepository
+﻿using CollectX.Domain.Commons;
+using System.Linq.Expressions;
+
+namespace CollectX.Data.IRepositories;
+public interface IRepository<T> where T : Auditable
 {
+    ValueTask<T> InsertAsync(T entity);
+    T Update(T entity);
+    IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null);
+    ValueTask<T> SelectAsync(Expression<Func<T, bool>> expression);
+    ValueTask<bool> DeleteAsync(Expression<Func<T, bool>> expression);
+    ValueTask SaveAsync();
 }
