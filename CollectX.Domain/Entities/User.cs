18c1edd5dde01dc4b6404cafcb228bd64c111e40
azimochilov <azimochilov226@gmail.com>
2023-06-21 13:16:37 +0300

    user added

--- CollectX.Domain/Entities/User.cs
+++ CollectX.Domain/Entities/User.cs
@@ -3,5 +3,9 @@
 namespace CollectX.Domain.Entities;
 public class User : Auditable
 {
-
+    public string FirstName { get; set; }    
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
