5a93f96df6550472fddbf78aaa0103be767a45e6
azimochilov <azimochilov226@gmail.com>
2023-06-21 16:20:20 +0300

    comment result dto added fully

--- CollectX.Service/DTOs/Comments/CommentResultDto.cs
+++ CollectX.Service/DTOs/Comments/CommentResultDto.cs
@@ -1,4 +1,7 @@
 ﻿namespace CollectX.Service.DTOs.Comments;
 internal class CommentResultDto
 {
+    public string Text { get; set; }
+    public long ItemId { get; set; }
+    public long UserId { get; set; }
 }
