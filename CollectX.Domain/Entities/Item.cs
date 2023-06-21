6c6af474e1c13ad264b22b4773dcfe38631963d6
azimochilov <azimochilov226@gmail.com>
2023-06-21 14:26:53 +0300

    updated item entity

--- CollectX.Domain/Entities/Item.cs
+++ CollectX.Domain/Entities/Item.cs
@@ -6,5 +6,8 @@ public class Item : Auditable
     public string Name { get; set; }
     public List<string> Tags { get; set; }
     public List<CustomField> CustomFields { get; set; }
-
+    public List<Like> Likes { get; set; }
+    public List<Comment> Comments { get; set; }
+    public long UserId { get; set; }
+    public User User { get; set; }
 }
