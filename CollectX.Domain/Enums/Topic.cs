cc873b9585ac11996581c1dfaa7069286c25c2e1
azimochilov <azimochilov226@gmail.com>
2023-06-21 13:01:14 +0300

    topic enum added

---
+++ CollectX.Domain/Enums/Topic.cs
@@ -0,0 +1,27 @@
+﻿namespace CollectX.Domain.Enums;
+public enum Topic : byte
+{
+    Books,
+    PostStamps,
+    Whiskeys,
+    Coins,
+    Paintings,
+    Antiques,
+    Comics,
+    Movies,
+    Music,
+    VideoGames,
+    SportsMemorabilia,
+    TradingCards,
+    Jewelry,
+    Toys,
+    Autographs,
+    CollectibleFigurines,
+    Wine,
+    VinylRecords,
+    Photography,
+    Fashion,
+    Pottery,
+    Sculptures,
+
+}
