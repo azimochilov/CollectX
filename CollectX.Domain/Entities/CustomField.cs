8aa0b10b8423aa442ad9c483de6bbeb26c9d87e4
azimochilov <azimochilov226@gmail.com>
2023-06-21 13:48:14 +0300

    custom field added

---
+++ CollectX.Domain/Entities/CustomField.cs
@@ -0,0 +1,8 @@
+﻿using CollectX.Domain.Commons;
+
+namespace CollectX.Domain.Entities;
+public class CustomField : Auditable
+{
+    public string Name { get; set; }
+    public object Value { get; set; }
+}
