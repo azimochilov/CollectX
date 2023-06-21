0ccf4e6166e2c2b66e5cf7c6760b07759eb61d24
azimochilov <azimochilov226@gmail.com>
2023-06-21 15:03:54 +0300

    added tables

--- CollectX.Data/DbContexts/AppDbContext.cs
+++ CollectX.Data/DbContexts/AppDbContext.cs
@@ -10,4 +10,12 @@ public class AppDbContext : DbContext
     {
     }
     public DbSet<User> Users { get; set; }
+    public DbSet<Like> Likes { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Collection> Collections { get; set; }
+    public DbSet<CustomField> CustomFields { get; set; }
+
+
+
 }
