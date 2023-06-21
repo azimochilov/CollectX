e61eb5c93a113be8583d103ac0cd52bd06104805
azimochilov <azimochilov226@gmail.com>
2023-06-21 14:16:27 +0300

    comment is done

---
+++ CollectX.Domain/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿using CollectX.Domain.Commons;
+
+namespace CollectX.Domain.Entities;
+public class Comment : Auditable
+{
+    public string Text { get; set; }
+    public long ItemId { get; set; }
+    public Item Item { get; set; }
+    public long UserId { get; set; }
+    public User User { get; set; }
+
+}
