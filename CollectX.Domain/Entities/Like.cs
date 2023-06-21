943adf1166eb23f2f413e13332eb88e6124199db
azimochilov <azimochilov226@gmail.com>
2023-06-21 14:18:01 +0300

    like is done

---
+++ CollectX.Domain/Entities/Like.cs
@@ -0,0 +1,11 @@
+﻿using CollectX.Domain.Commons;
+
+namespace CollectX.Domain.Entities;
+public class Like : Auditable
+{
+    public long ItemId { get; set; }
+    public Item Item { get; set; }
+    public long UserId { get; set; }
+    public User User { get; set; }
+
+}
