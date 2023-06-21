0fb1a6c191be35f4a27dc3778a449ecc40e41f5d
azimochilov <azimochilov226@gmail.com>
2023-06-21 14:49:54 +0300

    packages installed

--- CollectX.Data/DbContexts/AppDbContext.cs
+++ CollectX.Data/DbContexts/AppDbContext.cs
@@ -1,5 +1,13 @@
-﻿namespace CollectX.Data.DbContexts;
-public class AppDbContext
-{
+﻿using CollectX.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
+namespace CollectX.Data.DbContexts;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
 }
