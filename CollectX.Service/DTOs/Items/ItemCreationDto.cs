1446b9669f2a937915f9bb72a7d9f1a389cb2798
azimochilov <azimochilov226@gmail.com>
2023-06-21 16:13:42 +0300

    item creation and result dto are done

---
+++ CollectX.Service/DTOs/Items/ItemCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using CollectX.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CollectX.Service.DTOs.Items;
+public class ItemCreationDto
+{
+    [Required(ErrorMessage = "Name")]
+    public string Name { get; set; }
+    public List<string> Tags { get; set; }
+    public List<CustomField> CustomFields { get; set; }
+    public List<Like> Likes { get; set; }
+    public List<Comment> Comments { get; set; }
+    public long UserId { get; set; }
+}
---
+++ CollectX.Service/DTOs/Items/ItemResultDto.cs
@@ -0,0 +1,12 @@
+﻿using CollectX.Domain.Entities;
+
+namespace CollectX.Service.DTOs.Items;
+public class ItemResultDto
+{
+    public string Name { get; set; }
+    public List<string> Tags { get; set; }
+    public List<CustomField> CustomFields { get; set; }
+    public List<Like> Likes { get; set; }
+    public List<Comment> Comments { get; set; }
+    public long UserId { get; set; }
+}
