26dc8dff0024567f9204d60c160f4aa016ee393e
CS <azimochilov226@gmail.com>
2023-07-06 15:12:52 +0300

    userservice is stared

--- CollectX.Service/Service/Users/UserService.cs
+++ CollectX.Service/Service/Users/UserService.cs
@@ -1,6 +1,36 @@
-﻿using CollectX.Service.Interface.Users;
+﻿using CollectX.Service.DTOs.Users;
+using CollectX.Service.Interface.Users;
 
 namespace CollectX.Service.Service.Users;
 public class UserService : IUserService
 {
+    public Task<UserResultDto> AddAsync(UserCreationDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResultDto> ChangePasswordAsync(UserChangePasswordDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResultDto> ModifyAsync(long id, UserUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoveAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<UserResultDto>> RetrieveAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResultDto> RetrieveByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
 }
